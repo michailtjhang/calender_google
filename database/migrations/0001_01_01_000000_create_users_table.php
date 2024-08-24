9a350f7d18393f0484a4391f8141bfc9c4adc42d
michailtjhang <michailaja@gmail.com>
2024-08-24 10:06:38 +0700

    feature: add auth ui laravel

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('calendar_access_token')->nullable();
+            $table->text('calendar_refresh_token')->nullable();
+            $table->string('calendar_user_account_info')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
